2e1451c02be37138b1768464b5d09001d18d3e73
Ashish Singh Rawat <ashishsrawat98@gmail.com>
2022-09-05 15:24:55 +0530

    'Deploying'

--- server.js
+++ server.js
@@ -36,6 +36,17 @@ app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 
+//Server static assets if production
+if(process.env.NODE_ENV === 'production') {
+  // set static folder
+
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
