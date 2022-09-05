81b4243af5ede0ec31f569763256404f81afa1af
Ashish Singh Rawat <ashishsrawat98@gmail.com>
2022-09-05 15:42:19 +0530

    'Deploying'

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require('path');
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
